ef903a603a81a7b936a1f2e71ed9026de3c84244
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-19 18:33:26 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,15 @@
 class Customer
+  attr_accessor :name, :age 
+  @@all = []
+  
+  def initialize(name, age) 
+    self.name = name 
+    self.age = age 
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all
+  end
   
 end
