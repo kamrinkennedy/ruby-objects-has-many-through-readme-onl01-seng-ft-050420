90b5792b74c0c0d3d4c8ee19ad56cdd269b495f9
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-19 18:34:25 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,11 @@
 class Waiter
-
+  attr_accessor :name, :yrs_experience
+  @@all = []
+  
+  def initialize(name, yrs_experience)
+    self.name = name 
+    self.yrs_experience = yrs_experience
+  end
+  
+  
 end
